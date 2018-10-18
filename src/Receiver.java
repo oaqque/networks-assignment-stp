5ae39d8da3415d2f6e66692fc2e2e9782416ccb7
oaqque <oaqque@me.com>
2018-10-18 17:22:35 +1100

    Receiver server is now ready to ACK Packets that are sent to it... Data still not stored.

--- src/Receiver.java
+++ src/Receiver.java
@@ -10,31 +10,81 @@ public class Receiver {
     private static DatagramSocket receiverSocket;
     private static InetAddress sourceAddress;
     private static int sourcePort;
+    private static int currentSeqNum;
+    private static int currentAckNum;
 
     private static final int HEADER_SIZE = 9;
     private static final int ACK_FLAG = 0;
     private static final int SYN_FLAG = 1;
     private static final int FIN_FLAG = 2;
 
-    public static void main(String[] args) throws IOException {
-        // First grab the arguments from the command line ensuring that there are 2.
+    public static void main (String[] args) throws IOException {
+        // First grab the arguments from the command line ensuring that there are 2
         if (args.length != 2) {
             System.out.println("Required arguments: receiver_port, file_r.pdf");
             return;
         } else {
+            // If there are no issues with the arguments provided in the command line then bootstrap the Receiver
+            // server
             if (!bootstrapReceiver(args)) {
                 System.out.println("Failed to bootstrap the receiver.");
                 return;
             }
         }
 
-        if(!handshake()) {
+        // Start the three-way handshake process with the source
+        if (!handshake()) {
             System.out.println("Failed to complete handshake with client.");
             return;
         }
+
+        DatagramPacket dataPacket = new DatagramPacket(new byte[1024], 1024); // TODO comment on this
+        STP packetSTP;
+        byte[] packetHeader;
+        int segmentSize;
+
+        // After handshake has been completed, keep the server waiting for packets
+        while (true) {
+
+            System.out.println("Block while waiting for data packet...");
+            receiverSocket.receive(dataPacket);
+            packetSTP = getHeaderFromPacket(dataPacket);
+            packetHeader = packetSTP.getHeader();
+
+            // After receiving the packet ensure that this is not the final packet. If it is then we break the loop
+            // of receiving packets and initiate shutdown.
+            if (checkSTPHeaderFlags(dataPacket, FIN_FLAG)) {
+                // TODO
+                break;
+            }
+            System.out.println("Packet is not FIN Packet...");
+
+            // Check if the packets are out of order...
+            if (currentAckNum != packetSTP.getSequenceNum()) {
+                // TODO
+            }
+            System.out.println("Packet is in order...");
+
+            // Since it is not a FIN Packet then we simply ACK the packet.
+            // First get the sequence number from the packet we just received and ACK back the sequence number + the
+            // number of bytes received. The number of bytes sent is the length of the UDP datapacket - the length of
+            // the STP header.
+
+            segmentSize = dataPacket.getLength() - packetHeader.length;
+            currentAckNum += segmentSize;
+
+            System.out.println("Creating ACK Packet...");
+            STP ackSegment = new STP(true, false, false, currentSeqNum, currentAckNum);
+            DatagramPacket ackPacket = new DatagramPacket(ackSegment.getHeader(), ackSegment.getHeader().length,
+                    sourceAddress, sourcePort);
+
+            receiverSocket.send(ackPacket);
+            System.out.println("ACK Packet successfully sent. Ack Num: " + currentAckNum);
+        }
+
     }
 
-    private static boolean bootstrapReceiver(String[] args) {
+    private static boolean bootstrapReceiver (String[] args) {
         receiverPort = Integer.parseInt(args[0]);
         fileName = args[1];
 
@@ -49,13 +99,13 @@ public class Receiver {
         return true;
     }
 
-    private static boolean handshake() throws IOException {
+    private static boolean handshake () throws IOException {
         // Create a Datagram Packet to store the incoming Syn Packet.
         DatagramPacket synPacket = new DatagramPacket(new byte[HEADER_SIZE], HEADER_SIZE);
 
         // Block while waiting for the initial SYN Packet to arrive.
         System.out.println("Block while waiting for SYN Packet to arrive...");
-        while(!checkSTPHeaderFlags(synPacket, SYN_FLAG)) {
+        while (!checkSTPHeaderFlags(synPacket, SYN_FLAG)) {
             receiverSocket.receive(synPacket);
         }
 
@@ -81,9 +131,10 @@ public class Receiver {
 
         // Block while waiting for final ACK Packet to signal that the three-way handshake is complete.
         System.out.println("Block while waiting for final ACK Packet to arrive...");
-        while(!checkSTPHeaderFlags(ackPacket, ACK_FLAG) && !checkSTPAckNum(ackPacket, receiverISN+1)) {
+        while (!checkSTPHeaderFlags(ackPacket, ACK_FLAG) && !checkSTPAckNum(ackPacket, receiverISN+1)) {
             receiverSocket.receive(ackPacket);
         }
+        currentSeqNum = receiverISN+1;
         System.out.println("ACK successfully received, three way handshake complete");
 
         return true;
@@ -95,7 +146,7 @@ public class Receiver {
      * @param flag
      * @return
      */
-    private static boolean checkSTPHeaderFlags(DatagramPacket packet, int flag) {
+    private static boolean checkSTPHeaderFlags (DatagramPacket packet, int flag) {
         STP header = getHeaderFromPacket(packet);
         return header.checkFlag(flag);
     }
@@ -106,7 +157,7 @@ public class Receiver {
      * @param ackNum
      * @return
      */
-    private static boolean checkSTPAckNum(DatagramPacket packet, int ackNum) {
+    private static boolean checkSTPAckNum (DatagramPacket packet, int ackNum) {
         STP header = getHeaderFromPacket(packet);
         return header.checkAckNum(ackNum);
     }
@@ -116,7 +167,7 @@ public class Receiver {
      * @param packet
      * @return
      */
-    private static STP getHeaderFromPacket(DatagramPacket packet) {
+    private static STP getHeaderFromPacket (DatagramPacket packet) {
         byte[] packetData = packet.getData();
         byte[] header = new byte[HEADER_SIZE];
         System.arraycopy(packetData, 0, header, 0, HEADER_SIZE);
